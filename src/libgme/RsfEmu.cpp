fd1c3768cbfc4009fb1c04f14afcf3d1f558eb61
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-08-29 07:07:37 +0400

    Test

--- src/libgme/RsfEmu.cpp
+++ src/libgme/RsfEmu.cpp
@@ -157,7 +157,7 @@ void RsfEmu::mWriteRegisters(blip_clk_time_t time) {
   uint16_t mask = get_be16(mIt++);
   for (unsigned addr = 0; mask != 0; mask >>= 1, addr++) {
     if (mask & 1)
-      mApu.Write(time, addr, *(++mIt));
+      mApu.Write(time += 20, addr, *(++mIt));
   }
 }
 
