a96c74f512ec7be8a33894ffcbed521819b3408e
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-08-30 23:17:52 +0400

    Fix

--- src/libgme/RsfEmu.cpp
+++ src/libgme/RsfEmu.cpp
@@ -142,7 +142,7 @@ void RsfEmu::mSetTempo(double t) {
 
 blargg_err_t RsfEmu::mStartTrack(int track) {
   RETURN_ERR(ClassicEmu::mStartTrack(track));
-  mNextPlay = 0;
+  mNextPlay = mPlayPeriod;
   mIt = mFile.begin;
   mApu.Reset();
   SetTempo(mGetTempo());
