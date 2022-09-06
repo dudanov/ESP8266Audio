3209201364f7f5420bcc146d40bf4be939566963
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-09-06 17:58:26 +0400

    Changes

--- src/libgme/ay/StcEmu.h
+++ src/libgme/ay/StcEmu.h
@@ -207,6 +207,7 @@ class StcEmu : public ClassicEmu {
     for (unsigned idx = 0; idx < 3; ++idx) {
       auto &c = mChannel[idx];
       c.SetPatternData(mModule->GetPatternData(pattern, idx));
+      c.SetOrnamentData(mModule->GetOrnamentData(0));
     }
     return true;
   }
