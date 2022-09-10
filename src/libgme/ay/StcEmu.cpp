9163ed998531ef43b06cf928c201f12fd6fe3893
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2022-09-10 18:12:53 +0400

    Changes

--- src/libgme/ay/StcEmu.cpp
+++ src/libgme/ay/StcEmu.cpp
@@ -21,20 +21,22 @@
 namespace gme {
 namespace emu {
 namespace ay {
+namespace stc {
 
 static const uint32_t CLK_SPECTRUM = 3546900;
 
-inline uint16_t StcEmu::Channel::GetTonePeriod(uint8_t tone) {
-  static const uint16_t TABLE[] PROGMEM = {
-      0x0EF8, 0x0E10, 0x0D60, 0x0C80, 0x0BD8, 0x0B28, 0x0A88, 0x09F0, 0x0960, 0x08E0, 0x0858, 0x07E0, 0x077C, 0x0708,
-      0x06B0, 0x0640, 0x05EC, 0x0594, 0x0544, 0x04F8, 0x04B0, 0x0470, 0x042C, 0x03F0, 0x03BE, 0x0384, 0x0358, 0x0320,
-      0x02F6, 0x02CA, 0x02A2, 0x027C, 0x0258, 0x0238, 0x0216, 0x01F8, 0x01DF, 0x01C2, 0x01AC, 0x0190, 0x017B, 0x0165,
-      0x0151, 0x013E, 0x012C, 0x011C, 0x010B, 0x00FC, 0x00EF, 0x00E1, 0x00D6, 0x00C8, 0x00BD, 0x00B2, 0x00A8, 0x009F,
-      0x0096, 0x008E, 0x0085, 0x007E, 0x0077, 0x0070, 0x006B, 0x0064, 0x005E, 0x0059, 0x0054, 0x004F, 0x004B, 0x0047,
-      0x0042, 0x003F, 0x003B, 0x0038, 0x0035, 0x0032, 0x002F, 0x002C, 0x002A, 0x0027, 0x0025, 0x0023, 0x0021, 0x001F,
-      0x001D, 0x001C, 0x001A, 0x0019, 0x0017, 0x0016, 0x0015, 0x0013, 0x0012, 0x0011, 0x0010, 0x000F,
-  };
-  return pgm_read_word(TABLE + ((tone <= 95) ? tone : 95));
+const uint16_t STCModule::NOTE_TABLE[] PROGMEM = {
+    0x0EF8, 0x0E10, 0x0D60, 0x0C80, 0x0BD8, 0x0B28, 0x0A88, 0x09F0, 0x0960, 0x08E0, 0x0858, 0x07E0, 0x077C, 0x0708,
+    0x06B0, 0x0640, 0x05EC, 0x0594, 0x0544, 0x04F8, 0x04B0, 0x0470, 0x042C, 0x03F0, 0x03BE, 0x0384, 0x0358, 0x0320,
+    0x02F6, 0x02CA, 0x02A2, 0x027C, 0x0258, 0x0238, 0x0216, 0x01F8, 0x01DF, 0x01C2, 0x01AC, 0x0190, 0x017B, 0x0165,
+    0x0151, 0x013E, 0x012C, 0x011C, 0x010B, 0x00FC, 0x00EF, 0x00E1, 0x00D6, 0x00C8, 0x00BD, 0x00B2, 0x00A8, 0x009F,
+    0x0096, 0x008E, 0x0085, 0x007E, 0x0077, 0x0070, 0x006B, 0x0064, 0x005E, 0x0059, 0x0054, 0x004F, 0x004B, 0x0047,
+    0x0042, 0x003F, 0x003B, 0x0038, 0x0035, 0x0032, 0x002F, 0x002C, 0x002A, 0x0027, 0x0025, 0x0023, 0x0021, 0x001F,
+    0x001D, 0x001C, 0x001A, 0x0019, 0x0017, 0x0016, 0x0015, 0x0013, 0x0012, 0x0011, 0x0010, 0x000F,
+};
+
+inline uint16_t Channel::GetTonePeriod(uint8_t tone) {
+  return pgm_read_word(STCModule::NOTE_TABLE + ((tone <= 95) ? tone : 95));
 }
 
 StcEmu::StcEmu() {
@@ -43,7 +45,7 @@ StcEmu::StcEmu() {
   mSetType(gme_stc_type);
   mSetChannelsNames(CHANNELS_NAMES);
   mSetChannelsTypes(CHANNELS_TYPES);
-  mSetSilenceLookahead(6);
+  mSetSilenceLookahead(1);
 }
 
 StcEmu::~StcEmu() {}
@@ -104,74 +106,70 @@ void StcEmu::mInit() {
   mPositionIt = mModule->GetPositionBegin();
   memset(&mChannels, 0, sizeof(mChannels));
   auto pattern = mModule->GetPattern(mPositionIt->pattern);
-  for (unsigned idx = 0; idx < 3; ++idx) {
-    auto &channel = mChannels[idx];
-    channel.SetPatternData(mModule->GetPatternData(pattern, idx));
-    channel.SetOrnament(mModule->GetOrnament(0));
+  for (uint8_t idx = 0; idx != mChannels.size(); ++idx) {
+    Channel &c = mChannels[idx];
+    c.SetPatternData(mModule->GetPatternData(pattern, idx));
+    c.SetOrnament(mModule->GetOrnament(0));
   }
 }
 
 /* STC MODULE */
 
-inline int16_t StcEmu::SampleData::Transposition() const {
+inline int16_t SampleData::Transposition() const {
   int16_t result = mData[0] / 16 * 256 + mData[2];
   return (mData[1] & 32) ? result : -result;
 }
 
-inline const StcEmu::Position *StcEmu::STCModule::GetPositionBegin() const {
-  return mGetPointer<PositionsTable>(mPositions)->position;
-}
+inline const Position *STCModule::GetPositionBegin() const { return mGetPointer<PositionsTable>(mPositions)->position; }
 
-inline const StcEmu::Position *StcEmu::STCModule::GetPositionEnd() const {
+inline const Position *STCModule::GetPositionEnd() const {
   auto p = mGetPointer<PositionsTable>(mPositions);
   return p->position + p->count + 1;
 }
 
-inline size_t StcEmu::STCModule::mGetPositionsCount() const {
-  return mGetPointer<PositionsTable>(mPositions)->count + 1;
-}
+inline size_t STCModule::mGetPositionsCount() const { return mGetPointer<PositionsTable>(mPositions)->count + 1; }
 
-inline const uint8_t *StcEmu::STCModule::GetPatternData(const StcEmu::Pattern *pattern, uint8_t channel) const {
+inline const uint8_t *STCModule::GetPatternData(const Pattern *pattern, uint8_t channel) const {
   return mGetPointer<uint8_t>(pattern->DataOffset(channel));
 }
 
-inline const uint8_t *StcEmu::STCModule::GetPatternData(uint8_t pattern, uint8_t channel) const {
+inline const uint8_t *STCModule::GetPatternData(uint8_t pattern, uint8_t channel) const {
   return GetPatternData(GetPattern(pattern), channel);
 }
 
-inline const StcEmu::Pattern *StcEmu::STCModule::mGetPatternBegin() const { return mGetPointer<Pattern>(mPatterns); }
+inline const Pattern *STCModule::mGetPatternBegin() const { return mGetPointer<Pattern>(mPatterns); }
 
-inline const StcEmu::Pattern *StcEmu::STCModule::GetPattern(uint8_t number) const {
+inline const Pattern *STCModule::GetPattern(uint8_t number) const {
   auto it = mGetPatternBegin();
   while (!it->HasNumber(number))
     ++it;
   return it;
 }
 
-inline const StcEmu::Ornament *StcEmu::STCModule::GetOrnament(uint8_t number) const {
+inline const Ornament *STCModule::GetOrnament(uint8_t number) const {
   auto it = mGetPointer<Ornament>(mOrnaments);
   while (!it->HasNumber(number))
     ++it;
   return it;
 }
 
-inline const StcEmu::Sample *StcEmu::STCModule::GetSample(uint8_t number) const {
+inline const Sample *STCModule::GetSample(uint8_t number) const {
   auto it = mSamples;
   while (!it->HasNumber(number))
     ++it;
   return it;
 }
 
-bool StcEmu::STCModule::mCheckPatternTable() const {
+bool STCModule::mCheckPatternTable() const {
   auto it = mGetPatternBegin();
-  for (uint8_t n = 0; n < Pattern::MAX_COUNT; ++n, ++it) {
+  for (uint8_t n = 0; n != Pattern::MAX_COUNT; ++n, ++it) {
     if (it->HasNumber(0xFF))
       return true;
   }
   return false;
 }
 
-const StcEmu::Pattern *StcEmu::STCModule::mFindPattern(uint8_t number) const {
+const Pattern *STCModule::mFindPattern(uint8_t number) const {
   for (auto it = mGetPatternBegin(); it != mGetPatternEnd(); ++it) {
     if (it->HasNumber(number))
       return it;
@@ -179,7 +177,7 @@ const StcEmu::Pattern *StcEmu::STCModule::mFindPattern(uint8_t number) const {
   return nullptr;
 }
 
-uint8_t StcEmu::STCModule::mCountPatternLength(const StcEmu::Pattern *pattern, uint8_t channel) const {
+uint8_t STCModule::mCountPatternLength(const Pattern *pattern, uint8_t channel) const {
   unsigned length = 0, skip = 0;
   for (auto it = GetPatternData(pattern, channel); *it != 0xFF; ++it) {
     const uint8_t data = *it;
@@ -199,13 +197,13 @@ uint8_t StcEmu::STCModule::mCountPatternLength(const StcEmu::Pattern *pattern, u
   return (length <= 64) ? length : 0;
 }
 
-bool StcEmu::STCModule::mCheckSongData() const {
+bool STCModule::mCheckSongData() const {
   uint8_t PatternLength = 0;
   for (auto PositionIt = GetPositionBegin(); PositionIt != GetPositionEnd(); ++PositionIt) {
     auto pattern = mFindPattern(PositionIt->pattern);
     if (pattern == nullptr)
       return false;
-    for (uint8_t channel = 0; channel < 3; ++channel) {
+    for (uint8_t channel = 0; channel != AyApu::OSCS_NUM; ++channel) {
       const uint8_t length = mCountPatternLength(pattern, channel);
       if (length == 0)
         return false;
@@ -220,7 +218,7 @@ bool StcEmu::STCModule::mCheckSongData() const {
   return true;
 }
 
-bool StcEmu::STCModule::CheckIntegrity(size_t size) const {
+bool STCModule::CheckIntegrity(size_t size) const {
   // Header size
   if (size <= sizeof(STCModule))
     return false;
@@ -263,12 +261,12 @@ bool StcEmu::STCModule::CheckIntegrity(size_t size) const {
   return true;
 }
 
-unsigned StcEmu::STCModule::CountSongLength() const {
+unsigned STCModule::CountSongLength() const {
   // all patterns has same length
-  return mCountPatternLength(GetPattern(GetPositionBegin()->pattern)) * mGetPositionsCount();
+  return mCountPatternLength(GetPattern(GetPositionBegin()->pattern)) * mGetPositionsCount() * mDelay;
 }
 
-inline bool StcEmu::Channel::IsEmptyLocation() {
+inline bool Channel::IsEmptyLocation() {
   if (mSkipCounter > 0) {
     mSkipCounter--;
     return true;
@@ -283,11 +281,7 @@ void StcEmu::mPlayPattern() {
       continue;
     while (true) {
       const uint8_t code = channel.PatternCode();
-      if (code == 0xFF) {
-        // End pattern marker. Advance to next song position and update all channels.
-        mAdvancePosition();
-        continue;
-      } else if (code <= 0x5F) {
+      if (code <= 0x5F) {
         // Note in semitones (00=C-1). End position.
         channel.SetNote(code);
         break;
@@ -315,6 +309,9 @@ void StcEmu::mPlayPattern() {
         channel.SetOrnament(mModule->GetOrnament(0));
         mApu.Write(mEmuTime, 13, code % 16);
         mApu.Write(mEmuTime, 11, channel.PatternCode());
+      } else if (code == 0xFF) {
+        // End pattern marker. Advance to next song position and update all channels.
+        mAdvancePosition();
       } else {
         // Number of empty locations after the subsequent code (0-63).
         channel.SetSkipCount(code - 0xA1);
@@ -329,13 +326,13 @@ inline void StcEmu::mAdvancePosition() {
     mSetTrackEnded();
   }
   auto pattern = mModule->GetPattern(mPositionIt->pattern);
-  for (unsigned idx = 0; idx < 3; ++idx)
+  for (unsigned idx = 0; idx != mChannels.size(); ++idx)
     mChannels[idx].SetPatternData(mModule->GetPatternData(pattern, idx));
 }
 
 void StcEmu::mPlaySamples() {
   uint8_t mixer = 0;
-  for (uint8_t idx = 0; idx < 3; ++idx, mixer >>= 1) {
+  for (uint8_t idx = 0; idx != mChannels.size(); ++idx, mixer >>= 1) {
     Channel &channel = mChannels[idx];
 
     if (!channel.IsEnabled()) {
@@ -362,7 +359,7 @@ void StcEmu::mPlaySamples() {
   mApu.Write(mEmuTime, 7, mixer);
 }
 
-inline void StcEmu::Channel::AdvanceSample() {
+inline void Channel::AdvanceSample() {
   if (--mSampleCounter) {
     ++mSamplePosition;
   } else if (mSample->IsRepeatable()) {
@@ -389,11 +386,12 @@ blargg_err_t StcEmu::mRunClocks(blip_clk_time_t &duration) {
   return nullptr;
 }
 
+}  // namespace stc
 }  // namespace ay
 }  // namespace emu
 }  // namespace gme
 
 static const gme_type_t_ gme_stc_type_ = {
-    "ZX Spectrum", 1, 0, &gme::emu::ay::StcEmu::createStcEmu, &gme::emu::ay::StcFile::createStcFile, "STC", 1,
+    "ZX Spectrum", 1, 0, &gme::emu::ay::stc::StcEmu::createStcEmu, &gme::emu::ay::stc::StcFile::createStcFile, "STC", 1,
 };
 extern gme_type_t const gme_stc_type = &gme_stc_type_;
--- src/libgme/ay/StcEmu.h
+++ src/libgme/ay/StcEmu.h
@@ -8,6 +8,172 @@
 namespace gme {
 namespace emu {
 namespace ay {
+namespace stc {
+
+struct SampleData {
+  uint8_t Volume() const { return mData[0] % 16; }
+  uint8_t Noise() const { return mData[1] % 32; }
+  bool ToneMask() const { return mData[1] & 64; }
+  bool NoiseMask() const { return mData[1] & 128; }
+  int16_t Transposition() const;
+
+ private:
+  uint8_t mData[3];
+};
+
+struct Sample {
+  bool HasNumber(uint8_t number) const { return mNumber == number; }
+  const SampleData *Data(size_t pos) const { return mData + pos; }
+  bool IsRepeatable() const { return mRepeatPosition != 0; }
+  uint8_t RepeatPosition() const { return mRepeatPosition; }
+  const uint8_t RepeatLength() const { return mRepeatLength; }
+
+ private:
+  uint8_t mNumber;
+  SampleData mData[32];
+  uint8_t mRepeatPosition;
+  uint8_t mRepeatLength;
+};
+
+struct Ornament {
+  bool HasNumber(uint8_t number) const { return mNumber == number; }
+  const uint8_t *Data() const { return mData; }
+
+ private:
+  uint8_t mNumber;
+  uint8_t mData[32];
+};
+
+struct Position {
+  uint8_t pattern;
+  uint8_t transposition;
+};
+
+struct PositionsTable {
+  uint8_t count;
+  Position position[0];
+};
+
+struct Pattern {
+  static const uint8_t MAX_COUNT = 32;
+  bool HasNumber(uint8_t number) const { return mNumber == number; }
+  const uint8_t *DataOffset(uint8_t channel) const { return mDataOffset[channel]; }
+
+ private:
+  uint8_t mNumber;
+  uint8_t mDataOffset[3][2];
+};
+
+struct Channel {
+  void SetNote(uint8_t note) {
+    mNote = note;
+    mSamplePosition = 0;
+    mSampleCounter = 32;
+  }
+  void Disable() { mSampleCounter = 0; }
+  bool IsEnabled() const { return mSampleCounter > 0; }
+
+  void SetPatternData(const uint8_t *data) { mPatternIt = data; }
+  uint8_t PatternCode() { return *mPatternIt++; }
+
+  void SetSample(const Sample *sample) { mSample = sample; }
+  void SetOrnament(const Ornament *ornament) { mOrnament = ornament->Data(); }
+  void AdvanceSample();
+
+  const SampleData *GetSampleData() const { return mSample->Data(mSamplePosition); }
+  uint8_t GetOrnamentNote() const { return mNote + mOrnament[mSamplePosition]; }
+
+  bool IsEnvelopeEnabled() const { return mEnvelope; }
+  void EnvelopeEnable() { mEnvelope = true; }
+  void EnvelopeDisable() { mEnvelope = false; }
+
+  void SetSkipCount(uint8_t delay) { mSkipCount = mSkipCounter = delay; }
+  bool IsEmptyLocation();
+
+  static uint16_t GetTonePeriod(uint8_t tone);
+
+ private:
+  // Pointer to sample.
+  const Sample *mSample;
+  // Pattern data iterator.
+  const uint8_t *mPatternIt;
+  // Pointer to ornament data.
+  const uint8_t *mOrnament;
+  uint8_t mNote;
+  uint8_t mSamplePosition;
+  uint8_t mSampleCounter;
+  uint8_t mSkipCounter;
+  uint8_t mSkipCount;
+  bool mEnvelope;
+};
+
+struct STCModule {
+  // Shared note period table.
+  static const uint16_t NOTE_TABLE[96];
+
+  // Get song global delay.
+  uint8_t GetDelay() const { return mDelay; }
+
+  // Begin position iterator.
+  const Position *GetPositionBegin() const;
+
+  // End position iterator.
+  const Position *GetPositionEnd() const;
+
+  // Get pattern by specified number.
+  const Pattern *GetPattern(uint8_t number) const;
+
+  // Get pattern data by specified number.
+  const uint8_t *GetPatternData(uint8_t pattern, uint8_t channel) const;
+
+  // Get data from specified pattern.
+  const uint8_t *GetPatternData(const Pattern *pattern, uint8_t channel) const;
+
+  // Get sample by specified number.
+  const Sample *GetSample(uint8_t number) const;
+
+  // Get data of specified ornament number.
+  const Ornament *GetOrnament(uint8_t number) const;
+
+  // Return song length in frames.
+  unsigned CountSongLength() const;
+
+  // Check file integrity.
+  bool CheckIntegrity(size_t size) const;
+
+ private:
+  template<typename T> const T *mGetPointer(const uint8_t *offset) const {
+    return reinterpret_cast<const T *>(&mDelay + get_le16(offset));
+  }
+
+  // Count pattern length. Return 0 on error.
+  uint8_t mCountPatternLength(const Pattern *pattern, uint8_t channel = 0) const;
+
+  // Check pattern table data by maximum records.
+  bool mCheckPatternTable() const;
+
+  // Check song data integrity (positions and linked patterns).
+  bool mCheckSongData() const;
+
+  // Find specified pattern by number. Return pointer to pattern on success, else nullptr.
+  const Pattern *mFindPattern(uint8_t pattern) const;
+
+  size_t mGetPositionsCount() const;
+
+  const Pattern *mGetPatternBegin() const;
+
+  const Pattern *mGetPatternEnd() const { return GetPattern(0xFF); }
+
+  /* STC MODULE HEADER DATA */
+
+  uint8_t mDelay;
+  uint8_t mPositions[2];
+  uint8_t mOrnaments[2];
+  uint8_t mPatterns[2];
+  char mName[18];
+  uint8_t mSize[2];
+  Sample mSamples[0];
+};
 
 class StcEmu : public ClassicEmu {
  public:
@@ -15,168 +181,6 @@ class StcEmu : public ClassicEmu {
   ~StcEmu();
   static MusicEmu *createStcEmu() { return BLARGG_NEW StcEmu; }
   static gme_type_t static_type() { return gme_stc_type; }
-
-  struct SampleData {
-    uint8_t Volume() const { return mData[0] % 16; }
-    uint8_t Noise() const { return mData[1] % 32; }
-    bool ToneMask() const { return mData[1] & 64; }
-    bool NoiseMask() const { return mData[1] & 128; }
-    int16_t Transposition() const;
-
-   private:
-    uint8_t mData[3];
-  };
-
-  struct Sample {
-    bool HasNumber(uint8_t number) const { return mNumber == number; }
-    const SampleData *Data(size_t pos) const { return mData + pos; }
-    bool IsRepeatable() const { return mRepeatPosition != 0; }
-    uint8_t RepeatPosition() const { return mRepeatPosition; }
-    const uint8_t RepeatLength() const { return mRepeatLength; }
-
-   private:
-    uint8_t mNumber;
-    SampleData mData[32];
-    uint8_t mRepeatPosition;
-    uint8_t mRepeatLength;
-  };
-
-  struct Ornament {
-    bool HasNumber(uint8_t number) const { return mNumber == number; }
-    const uint8_t *Data() const { return mData; }
-
-   private:
-    uint8_t mNumber;
-    uint8_t mData[32];
-  };
-
-  struct Position {
-    uint8_t pattern;
-    uint8_t transposition;
-  };
-
-  struct PositionsTable {
-    uint8_t count;
-    Position position[0];
-  };
-
-  struct Pattern {
-    static const uint8_t MAX_COUNT = 32;
-    bool HasNumber(uint8_t number) const { return mNumber == number; }
-    const uint8_t *DataOffset(uint8_t channel) const { return mDataOffset[channel]; }
-
-   private:
-    uint8_t mNumber;
-    uint8_t mDataOffset[3][2];
-  };
-
-  struct Channel {
-    void SetNote(uint8_t note) {
-      mNote = note;
-      mSamplePosition = 0;
-      mSampleCounter = 32;
-    }
-    void Disable() { mSampleCounter = 0; }
-    bool IsEnabled() const { return mSampleCounter > 0; }
-
-    void SetPatternData(const uint8_t *data) { mPatternIt = data; }
-    uint8_t PatternCode() { return *mPatternIt++; }
-
-    void SetSample(const Sample *sample) { mSample = sample; }
-    void SetOrnament(const Ornament *ornament) { mOrnament = ornament->Data(); }
-    void AdvanceSample();
-
-    const SampleData *GetSampleData() const { return mSample->Data(mSamplePosition); }
-    uint8_t GetOrnamentNote() const { return mNote + mOrnament[mSamplePosition]; }
-
-    bool IsEnvelopeEnabled() const { return mEnvelope; }
-    void EnvelopeEnable() { mEnvelope = true; }
-    void EnvelopeDisable() { mEnvelope = false; }
-
-    void SetSkipCount(uint8_t delay) { mSkipCount = mSkipCounter = delay; }
-    bool IsEmptyLocation();
-
-    static uint16_t GetTonePeriod(uint8_t tone);
-
-   private:
-    // Pointer to sample.
-    const Sample *mSample;
-    // Pattern data iterator.
-    const uint8_t *mPatternIt;
-    // Pointer to ornament data.
-    const uint8_t *mOrnament;
-    uint8_t mNote;
-    uint8_t mSamplePosition;
-    uint8_t mSampleCounter;
-    uint8_t mSkipCounter;
-    uint8_t mSkipCount;
-    bool mEnvelope;
-  };
-
-  struct STCModule {
-    // Get song global delay.
-    uint8_t GetDelay() const { return mDelay; }
-
-    // Begin position iterator.
-    const Position *GetPositionBegin() const;
-
-    // End position iterator.
-    const Position *GetPositionEnd() const;
-
-    // Get pattern by specified number.
-    const Pattern *GetPattern(uint8_t number) const;
-
-    // Get pattern data by specified number.
-    const uint8_t *GetPatternData(uint8_t pattern, uint8_t channel) const;
-
-    // Get data from specified pattern.
-    const uint8_t *GetPatternData(const Pattern *pattern, uint8_t channel) const;
-
-    // Get sample by specified number.
-    const Sample *GetSample(uint8_t number) const;
-
-    // Get data of specified ornament number.
-    const Ornament *GetOrnament(uint8_t number) const;
-
-    // Return song length in frames.
-    unsigned CountSongLength() const;
-
-    // Check file integrity.
-    bool CheckIntegrity(size_t size) const;
-
-   private:
-    template<typename T> const T *mGetPointer(const uint8_t *offset) const {
-      return reinterpret_cast<const T *>(&mDelay + get_le16(offset));
-    }
-
-    // Count pattern length. Return 0 on error.
-    uint8_t mCountPatternLength(const Pattern *pattern, uint8_t channel = 0) const;
-
-    // Check pattern table data by maximum records.
-    bool mCheckPatternTable() const;
-
-    // Check song data integrity (positions and linked patterns).
-    bool mCheckSongData() const;
-
-    // Find specified pattern by number. Return pointer to pattern on success, else nullptr.
-    const Pattern *mFindPattern(uint8_t pattern) const;
-
-    size_t mGetPositionsCount() const;
-
-    const Pattern *mGetPatternBegin() const;
-
-    const Pattern *mGetPatternEnd() const { return GetPattern(0xFF); }
-
-    /* STC MODULE HEADER DATA */
-
-    uint8_t mDelay;
-    uint8_t mPositions[2];
-    uint8_t mOrnaments[2];
-    uint8_t mPatterns[2];
-    char mName[18];
-    uint8_t mSize[2];
-    Sample mSamples[0];
-  };
 
  protected:
   blargg_err_t mLoad(const uint8_t *data, long size) override;
@@ -213,6 +217,7 @@ class StcEmu : public ClassicEmu {
   uint8_t mDelayCounter;
 };
 
+}  // namespace stc
 }  // namespace ay
 }  // namespace emu
 }  // namespace gme
